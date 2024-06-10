e67a1d18784050138e8748fceae19b63f72622a0
JrGustavo <gus@soypetal.com>
2024-06-10 17:29:23 -0500

    Update Alert

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,9 +1,41 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+const items = [
+    {
+        id: "item-1",
+        question: "Its it accessible?",
+        answer: "Yes. It adheres to the WAI-ARIA design pattern"
+    },
+    {
+        id: "item-2",
+        question: "Its it accessible?",
+        answer: "Yes. It adheres to the WAI-ARIA design pattern"
+    },
+    {
+        id: "item-3",
+        question: "Its it accessible?",
+        answer: "Yes. It adheres to the WAI-ARIA design pattern"
+    },
+];
+
+
 
 export default function Page() {
     return (
         <div>
-            <h1>Accordion</h1>
+            <Accordion type="single"  className="w-full">
+                {items.map((item) => (
+                    <AccordionItem value={item.id} key={item.id}>
+                    <AccordionTrigger>{item.question}</AccordionTrigger>
+                    <AccordionContent>{item.answer}</AccordionContent>
+                    </AccordionItem>
+            ))}
+            </Accordion>
         </div>
     )
 }
-
