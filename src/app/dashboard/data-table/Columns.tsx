2533822bc283871e12a434d3fd5e255d8e0803e2
JrGustavo <gus@soypetal.com>
2024-06-20 20:00:09 -0500

    Update DataTable

---
+++ src/app/dashboard/data-table/Columns.tsx
@@ -0,0 +1,5 @@
+export const Columns = () => {
+    return (
+        <></>
+    );
+};
---
+++ src/app/dashboard/data-table/page.tsx
@@ -0,0 +1,21 @@
+import {payments} from "@/app/data/payments.data";
+
+
+async function fetchData(){
+    return payments;
+}
+
+
+
+export default async function Page() {
+    const data = await fetchData();
+
+    return (
+        <div>
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
+        </div>
+    )
+}
+
